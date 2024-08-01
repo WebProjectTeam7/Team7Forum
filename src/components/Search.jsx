33fcb002ead646f74931eed0738d8e7518654957
YordanovDanail <166701329+YordanovDanail@users.noreply.github.com>
2024-08-01 18:24:02 +0300

    Adding search bar

---
+++ src/components/Search.jsx
@@ -0,0 +1,36 @@
+import { useContext, useState } from "react";
+import { AppContext } from "../state/app.context";
+
+export default function Search() {
+  const { searchQuery, setSearchQuery } = useContext(AppContext);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search by tag or name..."
+        value={inputValue}
+        onChange={handleInputChange}
+        onKeyPress={handleKeyPress}
+      />
+      <button onClick={handleSearch} style={{ padding: "5px" }}>
+        Search
+      </button>
+    </div>
+  );
+}
--- src/state/app.context.js
+++ src/state/app.context.js
@@ -1,7 +1,9 @@
 import { createContext } from "react";
 
 export const AppContext = createContext({
-    user: null,
-    userData: null,
-    setAppState: () => { },
+  user: null,
+  userData: null,
+  setAppState: () => {},
+  searchQuery: "",
+  setSearchQuery: () => {},
 });
