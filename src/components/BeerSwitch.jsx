e285f5c0dd1d68c9685c16de4e8edd961b8bb41b
YordanovDanail <166701329+YordanovDanail@users.noreply.github.com>
2024-08-04 16:53:59 +0300

    basic dark-light mode

--- src/components/BeerSwitch.jsx
+++ src/components/BeerSwitch.jsx
@@ -1,20 +1,35 @@
 import lightBeerIcon from '../image/light-beer.png';
 import darkBeerIcon from '../image/dark-beer.png';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './CSS/BeerSwitch.css';
 
 export default function BeerSwitch() {
-    const [isDarkBeer, setIsDarkBeer] = useState(false);
+    const [isDarkMode, setIsDarkMode] = useState(() => {
+        const savedMode = localStorage.getItem('theme');
+        return savedMode ? savedMode === 'dark' : false;
+    });
+
+    useEffect(() => {
+        if (isDarkMode) {
+            document.body.classList.add('dark-mode');
+            document.body.classList.remove('light-mode');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.body.classList.add('light-mode');
+            document.body.classList.remove('dark-mode');
+            localStorage.setItem('theme', 'light');
+        }
+    }, [isDarkMode]);
 
     const handleToggle = () => {
-        setIsDarkBeer(!isDarkBeer);
+        setIsDarkMode(!isDarkMode);
     };
 
     return (
         <div className="beer-slider">
             <img src={lightBeerIcon} alt="Light Beer Icon" className="icon light-beer-icon" />
             <label className="switch">
-                <input type="checkbox" checked={isDarkBeer} onChange={handleToggle} />
+                <input type="checkbox" checked={isDarkMode} onChange={handleToggle} />
                 <span className="slider"></span>
             </label>
             <img src={darkBeerIcon} alt="Dark Beer Icon" className="icon dark-beer-icon" />
