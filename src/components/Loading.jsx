d676f2b59fe315000e06ab19f7f62d79af05d0f7
Lilyana Palikova <lilyana.palikova1311@gmail.com>
2024-08-05 17:00:22 +0300

    edit loading jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,16 +1,36 @@
 /* eslint-disable indent */
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import loadingGif from '../image/loading.gif';
 import './CSS/Loading.css';
 
 const LoadingComponent = () => {
-  return (
-    <div className="loading">
-      <img src={loadingGif} alt="Loading..." />
-      <p>Loading...</p>
-    </div>
-  );
+  const [isLoading, setIsLoading] = useState(true);
+  // eslint-disable-next-line no-trailing-spaces
+  useEffect(() => {
+    const loading = async () => {
+      try {
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (error) {
+        console.error('Error during loading:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    loading();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <img src={loadingGif} alt="Loading..." />
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  return null;
 };
 
 export default LoadingComponent;
