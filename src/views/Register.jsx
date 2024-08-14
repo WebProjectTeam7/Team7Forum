851f0f998b6223cfc7e0cd2e2bacd17a26fde2ad
Lilyana Palikova <lilyana.palikova1311@gmail.com>
2024-08-14 09:42:59 +0300

    reg jsx

--- src/views/Register.jsx
+++ src/views/Register.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 import { useContext, useState } from 'react';
 import { registerUser } from '../services/auth.service';
 import { AppContext } from '../state/app.context';
@@ -8,7 +9,7 @@ import PasswordStrengthIndicator from '../components/PasswordStrength';
 import RoleEnum from '../common/role.enum';
 import InfoButton from '../components/InfoButton';
 import { FaEye } from 'react-icons/fa';
-import WelcomeGifNotification from '../components/NotificationAfterRegister';
+import WelcomeGifNotification  from '../components/NotificationAfterRegister';
 import ErrorComponent from '../components/ServerError';
 import './CSS/Modal.css';
 
@@ -49,7 +50,7 @@ export default function Register() {
 
         setLoading(true);
         setAlertMessage('');
-        setErrorMessage(null);
+        setErrorMessage('');
         setNoCredentials(false);
 
         const alertArr = [];
@@ -86,14 +87,7 @@ export default function Register() {
         }
 
         try {
-            const userFromDB = await getUserByUsername(user.username);
-            if (userFromDB) {
-                setAlertMessage(`User ${user.username} already exists!`);
-                setErrorMessage('User already exists.');
-                setShowGift(true);
-                setLoading(false);
-                return;
-            }
+            // TODO
             const credential = await registerUser(user.email, user.password);
             await createUser(user.username, credential.user.uid, user.email, user.firstName, user.lastName, user.role);
             setAppState({ user: credential.user, userData: null });
@@ -101,7 +95,7 @@ export default function Register() {
             setTimeout(() => {
                 setShowGift(false);
                 navigate('/');
-            }, 2000);
+            }, 6000);
         } catch (error) {
             setErrorMessage(error.message);
             setShowGift(true);
@@ -121,118 +115,117 @@ export default function Register() {
 
     return (
         <>
-            <div>
-                <form onSubmit={register}>
-                    <h1>Register</h1>
+         <div>
+            <form onSubmit={register}>
+                <h1>Register</h1>
 
-                    {/* USERNAME */}
-                    <label htmlFor="username">Username: </label>
-                    <InfoButton text=" Username must be:
+                {/* USERNAME */}
+                <label htmlFor="username">Username: </label>
+                <InfoButton text=" Username must be:
                                         between 4 to 32 characters,
                                         begin with a letter,
                                         consist of letters, numbers, underscores, or hyphens."/>
-                    <input
-                        value={user.username}
-                        onChange={updateUser('username')}
-                        type="text"
-                        name="username"
-                        id="username"
-                    />
+                <input
+                    value={user.username}
+                    onChange={updateUser('username')}
+                    type="text"
+                    name="username"
+                    id="username"
+                />
 
-                    <br /><br />
+                <br /><br />
 
-                    {/* EMAIL */}
-                    <label htmlFor="email">Email: </label>
-                    <InfoButton text=" Must be a valid email address." />
-                    <input
-                        value={user.email}
-                        onChange={updateUser('email')}
-                        type="text"
-                        name="email"
-                        id="email"
-                    />
-                    <br /><br />
+                {/* EMAIL */}
+                <label htmlFor="email">Email: </label>
+                <InfoButton text=" Must be a valid email address." />
+                <input
+                    value={user.email}
+                    onChange={updateUser('email')}
+                    type="text"
+                    name="email"
+                    id="email"
+                />
+                <br /><br />
 
-                    {/* FIRST NAME */}
-                    <label htmlFor="firstName">First Name: </label>
-                    <InfoButton text="First name must be:
+                {/* FIRST NAME */}
+                <label htmlFor="firstName">First Name: </label>
+                <InfoButton text="First name must be:
                                         between 4 to 32 characters,"/>
-                    <input
-                        value={user.firstName}
-                        onChange={updateUser('firstName')}
-                        type="text"
-                        name="firstName"
-                        id="firstName"
-                    />
-                    <br /><br />
+                <input
+                    value={user.firstName}
+                    onChange={updateUser('firstName')}
+                    type="text"
+                    name="firstName"
+                    id="firstName"
+                />
+                <br /><br />
 
-                    {/* LAST NAME */}
-                    <label htmlFor="lastName">Last Name: </label>
-                    <InfoButton text="Last name must be:
+                {/* LAST NAME */}
+                <label htmlFor="lastName">Last Name: </label>
+                <InfoButton text="Last name must be:
                                         between 4 to 32 characters,"/>
-                    <input
-                        value={user.lastName}
-                        onChange={updateUser('lastName')}
-                        type="text"
-                        name="lastName"
-                        id="lastName"
-                    />
+                <input
+                    value={user.lastName}
+                    onChange={updateUser('lastName')}
+                    type="text"
+                    name="lastName"
+                    id="lastName"
+                />
 
-                    <br /><br />
+                <br /><br />
 
-                    {/* PASSWORD */}
-                    <label htmlFor="password">Password: </label>
-                    <InfoButton text="Password must be between 5 to 30 characters.
+                {/* PASSWORD */}
+                <label htmlFor="password">Password: </label>
+                <InfoButton text="Password must be between 5 to 30 characters.
                                     Include uppercase and lowercase letters, numbers, and special characters
                                     to create a strong password."/>
-                    <input
-                        value={user.password}
-                        onChange={updateUser('password')}
-                        type={hidePassword ? 'password' : 'text'}
-                        name="password"
-                        id="password"
-                    />
-                    <a onClick={togglePasswordVisibility}>
-                        <FaEye style={{ color: !hidePassword ? '#FF0054' : '#c3c3c3' }} />
-                    </a>
-                    <PasswordStrengthIndicator password={user.password} />
-                    <br />
+                <input
+                    value={user.password}
+                    onChange={updateUser('password')}
+                    type={hidePassword ? 'password' : 'text'}
+                    name="password"
+                    id="password"
+                />
+                <a onClick={togglePasswordVisibility}>
+                    <FaEye style={{ color: !hidePassword ? '#FF0054' : '#c3c3c3' }} />
+                </a>
+                <PasswordStrengthIndicator password={user.password} />
+                <br />
 
-                    {/* CONFIRM PASSWORD */}
-                    <label htmlFor="confirmPassword">Confirm Password: </label>
-                    <input
-                        value={user.confirmPassword}
-                        onChange={updateUser('confirmPassword')}
-                        type={hidePassword ? 'password' : 'text'}
-                        name="confirmPassword"
-                        id="confirmPassword"
-                    /><br />
+                {/* CONFIRM PASSWORD */}
+                <label htmlFor="confirmPassword">Confirm Password: </label>
+                <input
+                    value={user.confirmPassword}
+                    onChange={updateUser('confirmPassword')}
+                    type={hidePassword ? 'password' : 'text'}
+                    name="confirmPassword"
+                    id="confirmPassword"
+                /><br />
 
-                    {/* SUBMIT */}
-                    <button>Register</button>
-                </form>
+                {/* SUBMIT */}
+                <button>Register</button>
+            </form>
                 {/* Modals for errors and notifications */}
-                {noCredentials && (
-                    <div className="modal-overlay">
-                        <div className="modal-content">
-                            <h2>Validation Error</h2>
-                            <p>{alertMessage}</p>
-                            <button onClick={closeModal}>OK</button>
-                        </div>
+            {noCredentials && (
+                <div className="modal-overlay">
+                    <div className="modal-content">
+                        <h2>Validation Error</h2>
+                        <p>{alertMessage}</p>
+                        <button onClick={closeModal}>OK</button>
                     </div>
-                )}
+                </div>
+            )}
 
-                {errorMessage && (
-                    <div className="modal-overlay">
-                        <div className="modal-content">
-                            <h2>Error</h2>
-                            <p>{errorMessage}</p>
-                            <button onClick={closeModal}>OK</button>
-                        </div>
+            {errorMessage && (
+                <div className="modal-overlay">
+                    <div className="modal-content">
+                        <h2>Error</h2>
+                        <p>{errorMessage}</p>
+                        <button onClick={closeModal}>OK</button>
                     </div>
-                )}
-                {errorMessage && <ErrorComponent />}
-                <WelcomeGifNotification show={showGift} onClose={handleCloseNotification} />
+                </div>
+            )}
+            <WelcomeGifNotification show={showGift} onClose={handleCloseNotification} />
             </div>
         </>
     );
